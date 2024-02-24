1454e7ba48b9866552645a2dcec96fde7af2a738
Jair Sanjuan <jairsanjuan21@hotmail.com>
2024-02-23 22:27:33 -0500

    hola

---
+++ Ejercicios_1/ejercicio_1.java
@@ -0,0 +1,13 @@
+package Ejercicios_1;
+import java.util.Scanner;
+/**Escribir un programa que pregunte al usuario su
+nombre y luego lo salude
+ */
+public class ejercicio_1 {
+    public static void main (String args []) {
+        Scanner teclado = new Scanner (System.in);
+        System.out.print ("Ingrese su nombre:  ");
+        String nom = teclado.nextLine ();
+        System.out.println ("Hola usuario: "+nom);
+    }
+}
---
+++ Ejercicios_1/ejercicio_2.java
@@ -0,0 +1,20 @@
+package Ejercicios_1;
+import java.util.Scanner;
+/**Escribir un programa que calcule el perimetro y
+área de un rectángulo dada su base y su altura
+*/
+public class ejercicio_2 {
+    public static void main (String args[]) {
+        Scanner teclado = new Scanner (System.in);
+        System.out.println ("Dame el valor de la base del rectangulo: ");
+        double base = teclado.nextDouble ();
+        System.out.println ("Dame el valor de la altura del rectangulo: ");
+        double altura = teclado.nextDouble ();
+        double area = base * altura;
+        double perimetro = 2*(base+altura);
+        System.out.println("El area de este rectangulo es: "+ area);
+        System.out.println("El perimetro de este rectangulo es: "+perimetro);
+    }
+
+
+}
