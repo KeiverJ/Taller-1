7892605ffc12ce2b71662c7eb81aa786a913a5b0
Jair Sanjuan <jairsanjuan21@hotmail.com>
2024-02-23 22:48:03 -0500

    hola

--- Ejercicios_1/ejercicio_3.java
+++ Ejercicios_1/ejercicio_3.java
@@ -9,11 +9,11 @@ public class ejercicio_3 {
         Scanner teclado = new Scanner (System.in);
         System.out.println ("Ingrese el valor de la base del rectangulo");
         double base = teclado.nextDouble();
-        System.out.printnl("Ingrese el valor de la altura del rectangunlo");
+        System.out.println("Ingrese el valor de la altura del rectangunlo");
         double altura = teclado.nextDouble ();
         double hyp = Math.sqrt(Math.pow(base, 2)+ Math.pow(altura, 2));
         System.out.println ("El valor de la hipotenusa del rectangulo es de: "+hyp);
-        
+
 
 
 
