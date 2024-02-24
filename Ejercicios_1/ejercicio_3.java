99e939f470ff5f60599e6605d8c403b88bf5743d
Jair Sanjuan <jairsanjuan21@hotmail.com>
2024-02-23 22:47:14 -0500

    hola

---
+++ Ejercicios_1/ejercicio_3.java
@@ -0,0 +1,21 @@
+package Ejercicios_1;
+import java.util.Scanner;
+/**Escribir un programa que nos devuelva la
+hipotenusa de un triángulo rectángulo conociendo
+sus catetos
+*/
+public class ejercicio_3 {
+    public static void main (String args[]) {
+        Scanner teclado = new Scanner (System.in);
+        System.out.println ("Ingrese el valor de la base del rectangulo");
+        double base = teclado.nextDouble();
+        System.out.printnl("Ingrese el valor de la altura del rectangunlo");
+        double altura = teclado.nextDouble ();
+        double hyp = Math.sqrt(Math.pow(base, 2)+ Math.pow(altura, 2));
+        System.out.println ("El valor de la hipotenusa del rectangulo es de: "+hyp);
+        
+
+
+
+    }
+}
---
+++ Ejercicios_1/ejercicio_4.java
@@ -0,0 +1,36 @@
+package Ejercicios_1;
+import java.util.Scanner;
+/**Realiza un programa que dados dos números
+muestre la suma resta división y multiplicación de
+ambos
+ */
+public class ejercicio_4 {
+    public static void main (String args []){
+        Scanner teclado = new Scanner (System.in);
+        System.out.println ("Ingrese el primer numero: ");
+        int num1 = teclado.nextInt ();
+        System.out.println ("Ingrese el segundo numero: ");
+        int num2 = teclado.nextInt ();
+        int suma = num1 + num2;
+        System.out.println ("La suma de los dos numeros es de: "+suma);
+        int mult = num1 * num2;
+        System.out.println("La multiplicacion de los dos numeros es de: "+mult);
+        if (num1>num2){
+            int resta = num1 - num2;
+            System.out.println("La resta de los dos numeros es de: "+resta);
+        } else {
+            int resta = num2 - num1;
+            System.out.println("La resta de los dos numeros es de: "+resta);
+        } 
+        if (num1 == 0 || num2 ==0){
+            System.out.println("Error, ningun numero es divisible por cero");
+        } else {
+            double div = (double) num1 / (double) num2;
+            System.out.println("La division de los dos numeros es de: "+div);
+        }
+
+
+
+
+    }
+}
